c5b8870725203b143ba87cb05c16cf15c9d453d6
Chris7848 <142909227+Chris7848@users.noreply.github.com>
2023-08-22 14:59:25 +0100

    Add files via upload

---
+++ chris.js
@@ -0,0 +1,19 @@
+// let name = ('chris');
+// console.log(name)
+// let department = ['computer','nursing','pharmacy','medicine']
+// console.log(department)
+// let scores = [23, 44, 56, 78]
+// console.log(scores) 
+
+// boolean
+
+let loggedin = true;
+
+let email = 'chris@gmail.com'
+// console.log(email.includes('@'));
+
+let firstNum = 45
+let secondNum = 44
+console.log(firstNum!=secondNum)
+
+
---
+++ jav.js
@@ -0,0 +1,52 @@
+// // // let text = " please leave";
+// // // console.log(text)
+// //let newtext = text.replace("leave","fish");
+// // // console.log(newtext)
+
+// // // let text = "2";
+// // // let from = text.padEnd(4,"x");
+// // // console.log(from)
+
+// //  let text = "fish egg";
+// // let fill = text.charCodeAt(0);
+// // console.log(fill)
+
+// // // let text = "d,o,c,um,e.nt,a.t,i,o,n";
+// // // let chris = text.split(".")
+// // // console.log(chris)
+
+// // // let text = "the rAin in spain falls mainly to gain";
+// // // let you = text.match("ain");
+// // // console.log(you)
+
+// // //includes   
+// //  let text = "i will hate you forever, till i die"   
+// // /let you = text.includes("die");   
+// // /console.log(you)   
+
+// let text = "from today till tomorrow"   
+//  let real = text.startsWith("till")   
+//  console.log(real)   
+
+// //  let text = `hes often called "jhonny"`   
+// // console.log(text)
+
+// //  let fix = "now, you, know";
+// //  console.log(fix)
+// //  let fish = fix.substr(6)
+// //  console.log(fish)
+
+// //  let dog = "       frog       ";
+// //  console.log(dog)
+// //  let cat = dog.trim();
+// // console.log(cat)
+
+
+// let oldtext = prompt(" what text do you want");
+//  let oldword = prompt("what word would you like to replace");
+//  let newword = prompt(" what word would you like to replace it with");
+//  let newtext = oldtext.replaceAll(oldword,newword)
+//  console.log(oldtext)
+//  console.log(newtext)
+
+
